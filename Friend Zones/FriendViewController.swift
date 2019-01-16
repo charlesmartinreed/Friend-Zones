ee002dc32e19ae17f65f2f0fa93b25d961f59d01
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-16 17:44:36 -0600

    Implement table view for editing friend info; displays selectable time zones and allows editing of name.

---
+++ Friend Zones/FriendViewController.swift
@@ -0,0 +1,112 @@
+//
+//  FriendViewController.swift
+//  Friend Zones
+//
+//  Created by Charles Martin Reed on 1/16/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class FriendViewController: UITableViewController {
+
+    //MARK:- Properties
+     //need bi-directional reference to previous screen, in form of a Friend object and a delegate
+    var friend: Friend!
+    let nameCellIdentifer = "NameCell"
+    let timeZoneCellIdentifer = "TimeZoneCell"
+    weak var delegate: ViewController?
+    
+    //array of time zones, index of selected time zone
+    var timeZones = [TimeZone]()
+    var selectedTimeZone = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //grab all the timezones, add them to the array
+        let identifers = TimeZone.knownTimeZoneIdentifiers
+        for identifer in identifers {
+            //create a timeZone instance from each identifer that we can work with
+            //timezones have interesting facts like, how far from GMT a location is
+            if let timeZone = TimeZone(identifier: identifer) {
+                timeZones.append(timeZone)
+            }
+        }
+        let now = Date()
+        
+        //sort our timeZones - 1. first by west to east, offset by GMT and 2. by name
+        timeZones.sort {
+            //get diff between first timezone and GMT
+            let ourDifference = $0.secondsFromGMT(for: now)
+            let otherDifference = $1.secondsFromGMT(for: now)
+            
+            if ourDifference == otherDifference {
+                //if same, sort by name
+                return $0.identifier < $1.identifier
+            } else {
+                //if different
+                return ourDifference < otherDifference
+            }
+        }
+        
+        selectedTimeZone = timeZones.index(of: friend.timeZone) ?? 0
+        
+    }
+    
+    //MARK:- IBActions
+    @IBAction func nameChanged(_ sender: UITextField) {
+        //calls the vc every time the key stroke is detected
+        
+    }
+    
+    //MARK:- table view delegate methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2 //name your friend and timezones
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return 1
+        } else {
+            return timeZones.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        //we're using a grouped style here
+        if section == 0 {
+            return "Name your friend"
+        } else {
+            return "Select their timezone"
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //load one of the two classes and show it on the screen
+        if indexPath.section == 0 {
+            //name your friend
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: nameCellIdentifer, for: indexPath) as? TextTableViewCell else {
+                fatalError("Couldn't create a TextTableViewCell")}
+            cell.textField.text = friend.name
+            return cell
+        } else {
+            //time zone
+            let cell = tableView.dequeueReusableCell(withIdentifier: timeZoneCellIdentifer, for: indexPath)
+            let timeZone = timeZones[indexPath.row]
+            
+            cell.textLabel?.text = timeZone.identifier
+            let timeDifference = timeZone.secondsFromGMT(for: Date()) / 3600
+            cell.detailTextLabel?.text = "\(timeDifference) hours GMT"
+            
+            //display to the user which time zone is currently selected
+            if indexPath.row == selectedTimeZone {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+            
+            return cell
+        }
+    }
+}
---
+++ Friend Zones/TextTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TextTableViewCell.swift
+//  Friend Zones
+//
+//  Created by Charles Martin Reed on 1/16/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class TextTableViewCell: UITableViewCell {
+
+    //MARK:- IBOutlets
+    @IBOutlet weak var textField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+
+}
